a9c7da0cbcc8653a03bd24a3a0d3943c035eb22b
marimokomoko <mari_n17@yahoo.co.jp>
2024-04-23 18:54:50 +0900

    [add]一覧画面作成

---
+++ app/src/main/java/com/example/memoapp/ui/home/HomeScreen.kt
@@ -0,0 +1,68 @@
+package com.example.memoapp.ui.home
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.memoapp.R
+import com.example.memoapp.ui.TodoTopAppBar
+import com.example.memoapp.ui.navigation.NavigationDestination
+
+object HomeDestination : NavigationDestination {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit = {},
+    navigateToItemUpdate: (Int) -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            TodoTopAppBar(
+                title = stringResource(R.string.app_name),
+                canNavigateBack = false
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(id = R.string.item_entry_title)
+                )
+            }
+        }) { innerPadding ->
+        Button(
+            onClick = {
+                navigateToItemUpdate(1)
+            }, modifier = Modifier.padding(innerPadding)
+        ) {
+            Text(text = "Edit画面")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeDestination() {
+    HomeScreen()
+}

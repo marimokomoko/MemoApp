686386ee52c94c348d322b8cfc0011add8a495db
marimokomoko <mari_n17@yahoo.co.jp>
2024-04-24 11:51:58 +0900

    [add]NavHostを取り込む

---
+++ app/src/main/java/com/example/memoapp/TodoApp.kt
@@ -0,0 +1,17 @@
+package com.example.memoapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.memoapp.ui.ui.navigation.TodoNavHost
+
+/**
+ * TodoApp:NavHostを取り込むコンポーザブル関数
+ */
+@Composable
+fun TodoApp(
+    navController: NavHostController = rememberNavController()
+) {
+    TodoNavHost(navController = navController, modifier = Modifier)
+}

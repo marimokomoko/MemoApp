4732e19ce2b39ba2cdc4f82478fc4ea279e95a07
marimokomoko <mari_n17@yahoo.co.jp>
2024-05-09 20:58:44 +0900

    [update]新規画面用のViewModel定義追加

--- app/src/main/java/com/example/memoapp/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/example/memoapp/ui/AppViewModelProvider.kt
@@ -7,14 +7,20 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.memoapp.TodoApplication
 import com.example.memoapp.ui.home.HomeViewModel
+import com.example.memoapp.ui.item.ItemEntryViewModel
 
 /**
  * AppViewModelProvider:viewModelFactory関数の結果を保持するオブジェクト
  */
 object AppViewModelProvider {
     val Factory = viewModelFactory {
+        // ホーム画面のViewModel初期化
         initializer {
             HomeViewModel(todoApplication().container.itemsRepository)
+        }
+        // 新規画面のViewModel初期化
+        initializer {
+            ItemEntryViewModel(todoApplication().container.itemsRepository)
         }
     }
 }

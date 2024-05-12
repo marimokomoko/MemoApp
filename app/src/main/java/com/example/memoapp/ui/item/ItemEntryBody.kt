607b328555dc22165d945fd1ddb203d9160539a3
marimokomoko <mari_n17@yahoo.co.jp>
2024-05-12 21:47:14 +0900

    [wip]新規画面ボタン追加

---
+++ app/src/main/java/com/example/memoapp/ui/item/ItemEntryBody.kt
@@ -0,0 +1,17 @@
+package com.example.memoapp.ui.item
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ItemEntryBody(
+    itemUiState: ItemUiState,
+    onItemValueChange: (ItemDetails) -> Unit,
+    onSaveClick: () -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+//    Column {
+//
+//    }
+}

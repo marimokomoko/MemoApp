9517f514a14c0cac1354f5d8222eefca2983544e
marimokomoko <mari_n17@yahoo.co.jp>
2024-05-15 20:37:40 +0900

    [update]新規画面viewModel組み込み

--- app/src/main/java/com/example/memoapp/ui/item/ItemEntryScreen.kt
+++ app/src/main/java/com/example/memoapp/ui/item/ItemEntryScreen.kt
@@ -1,16 +1,23 @@
 package com.example.memoapp.ui.item
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.memoapp.R
+import com.example.memoapp.ui.AppViewModelProvider
 import com.example.memoapp.ui.TodoTopAppBar
 import com.example.memoapp.ui.navigation.NavigationDestination
+import kotlinx.coroutines.launch
 
 /**
  * ItemEntryDestination:新規追加画面への画面遷移を表すオブジェクト
@@ -30,8 +37,11 @@ object ItemEntryDestination : NavigationDestination {
 fun ItemEntryScreen(
     navigateBack: () -> Unit = {},
     onNavigateUp: () -> Unit = {},
-    canNavigateBack: Boolean = true
+    canNavigateBack: Boolean = true,
+    viewModel: ItemEntryViewModel =
+            viewModel(factory = AppViewModelProvider.Factory)
 ) {
+    val coroutineScope = rememberCoroutineScope()
     Scaffold(
         topBar = {
             TodoTopAppBar(
@@ -41,6 +51,20 @@ fun ItemEntryScreen(
             )
         }
     ) { innerPadding ->
+        ItemEntryBody(
+            itemUiState = viewModel.itemUiState,
+            onItemValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.saveItem()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
         Text(text = "New memo画面", modifier = Modifier.padding(innerPadding))
     }
 }
